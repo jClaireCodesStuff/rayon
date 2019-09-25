9589aa60a62d71055d8ce862cef045dbacdd19ce
J Claire <jClaireCodesStuff@users.noreply.github.com>
2019-09-24 20:04:55 -0400

    Compile-fail test fixed, all existing tests either ported or not applicable.

--- rayon-futures/src/compile_fail/future_escape.rs
+++ rayon-futures/src/compile_fail/future_escape.rs
@@ -1,4 +1,4 @@
-/*! ```compile_fail,E0382,E0501,E0503,E0716
+/*! ```compile_fail,0382,0501,0503,0596,0597
 
 extern crate futures;
 extern crate rayon_core;
@@ -13,10 +13,15 @@ fn a() {
     let mut future = None;
     rayon_core::scope(|s| {
         let data = &mut *data;
-        future = Some(s.spawn_future(lazy(move || Ok::<_, ()>(&mut data[0]))));
+        // E0597 `data` does not live long enough.  It is captured by the
+        // future but does not outlive the invocation of `scope`
+        future = Some(s.spawn_future(async { &mut data[0] })); //~ ERROR;
     });
 
-    // `data` is still borrowed as part of future here:
+    // E0503 `*data` is mutably borrowed.  It is visible in the output type of
+    // the outer future, which will be dropped at the end of the function.
+    //
+    // E0501 `data[_]` was borrowed uniquely by closure passed to scope.
     assert_eq!(data[0], "Hello, world!"); //~ ERROR
 }
 
@@ -25,19 +30,25 @@ fn b() {
 
     let mut future = None;
     rayon_core::scope(|s| {
-        future = Some(s.spawn_future(lazy(move || Ok::<_, ()>(&mut data[0]))));
+        future = Some(s.spawn_future(async move { &mut data[0] } ));
     });
 
-    // `data` is moved into the scope above, can't use here
+    // E0382 borrow of moved value.  `data` was moved to satisfy the `async
+    // move` declaration
     assert_eq!(data[0], "Hello, world!"); //~ ERROR
 }
 
 fn c() {
     let mut future = None;
     // borrowed value does not live long enough
-    let data = &mut [format!("Hello, ")]; //~ ERROR
+    let data_store = [format!("Hello, ")];
+    let data = &mut data_store;
     rayon_core::scope(|s| {
-        future = Some(s.spawn_future(lazy(move || Ok::<_, ()>(&mut data[0]))));
+        // E0597 `data` does not live long enough`.  `data_store` is dropped
+        // before `future` (LIFO) but `future` may hold a reference to
+        // `data_store`.  This appears to be propagated to `data`, thus the
+        // error.
+        future = Some(s.spawn_future(async { &mut data[0] } )); //~ ERROR
     });
 }
 
